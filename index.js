bbb85d733dd45444ae069f023d82a62af0cb33a1
Aman Kumar <72601463+ak95305@users.noreply.github.com>
2021-05-21 18:53:33 +0530

    portfolio

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ AOS.init();
 
 let hamBurger = document.querySelector(".hamburger");
 let navBar = document.querySelector(".for-pc-view");
-let portfolio = document.querySelector(".portfolio");
+let portfolio = document.querySelector("#portfolio");
+let BG = document.querySelector(".bgblur");
 
 hamBurger.addEventListener("click", hamBurgerFunction);
 
@@ -11,10 +12,10 @@ function hamBurgerFunction(){
     if(navBar.classList.contains("mobile-nav-bar")){
         navBar.classList.remove("mobile-nav-bar");
         hamBurger.classList.remove("active-hamburger");
-        portfolio.style.display = "flex";
+        portfolio.style.opacity = "1";
     } else{
         navBar.classList.add("mobile-nav-bar");
         hamBurger.classList.add("active-hamburger");
-        portfolio.style.display = "none";
+        portfolio.style.opacity = "0";
     }
 }
