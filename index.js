7fd1ff465d99e911e96d24754b5d0811a0174dc0
Aman Kumar <72601463+ak95305@users.noreply.github.com>
2021-05-21 17:07:31 +0530

    Aman Portfolio

---
+++ index.js
@@ -0,0 +1,18 @@
+let hamBurger = document.querySelector(".hamburger");
+let navBar = document.querySelector(".for-pc-view");
+let portfolio = document.querySelector(".portfolio");
+
+hamBurger.addEventListener("click", hamBurgerFunction);
+
+
+function hamBurgerFunction(){
+    if(navBar.classList.contains("mobile-nav-bar")){
+        navBar.classList.remove("mobile-nav-bar");
+        hamBurger.classList.remove("active-hamburger");
+        portfolio.style.display = "flex";
+    } else{
+        navBar.classList.add("mobile-nav-bar");
+        hamBurger.classList.add("active-hamburger");
+        portfolio.style.display = "none";
+    }
+}
